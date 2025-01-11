eb7d30a924c7b8d41866db54f13c4f3a22108e16
lokeshyadav1401 <lokesh.2.yadav@coforge.com>
2025-01-11 19:16:24 +0530

    my project

--- dbt_demo/models/integration/dim_reviews.sql
+++
@@ -1,5 +0,0 @@
-select * 
-from 
---stg.airbnb.src_reviews
-{{ref('src_reviews')}}
-WHERE review_text is not null
--- dbt_demo/models/staging/src_host.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-id AS host_id,
-NAME AS host_name, 
-is_superhost, 
-created_at, 
-updated_at
-FROM
-{{source('raw_airbnb','hosts')}}
